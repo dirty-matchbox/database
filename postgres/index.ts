8418e5de5b0ac18c0c234c47fb9a79bdfeb7c16d
nemanja-sljivic-v1 <neno93993@gmail.com>
2024-12-17 00:19:47 +0100

    Update of factory

--- postgres/index.ts
+++ postgres/index.ts
@@ -1,5 +1,5 @@
 export * from "./lib/types";
 export * from "./di/types";
 export { default as PostgresDatabase } from "./lib/index";
-export * as postgresDatabaseFactory from "./di/factory";
+export {default as postgresDatabaseFactory} from "./di/factory";
 export { addToContainer as addPostgresDatabaseFactoryToContainer } from "./di/registerPostgresDatabaseFactory";
